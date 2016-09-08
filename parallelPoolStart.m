cc9030755be529b760f2d6e4339fd49efc01ec53
Raphael Petegrosso <raphaelpetegrosso@x-10-104-112-146.uofm-secure.wireless.umn.edu>
2016-09-08 15:09:32 -0500

    parallel pool start exception catching

--- parallelPoolStart.m
+++ parallelPoolStart.m
@@ -1,12 +1,15 @@
 function parallelPoolStart()
-    if exist('OCTAVE_VERSION', 'builtin') ~= 0
-        return;
-    else
-        if exist('parpool') == 2
-            parpool;
-        elseif exist('matlabpool')
-            matlabpool;
+    try
+        if exist('OCTAVE_VERSION', 'builtin') ~= 0
+            return;
+        else
+            if exist('parpool') == 2
+                parpool;
+            elseif exist('matlabpool')
+                matlabpool;
+            end
         end
+    catch
     end
 end
 
